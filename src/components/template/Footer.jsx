3dd63fecb6470b6b33776a6fc5f2d89dbdc73be1
Diego <lima.j.diego@gmail.com>
2021-07-07 12:17:52 -0300

    Aplicando o componente footer

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import './Footer.css'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>Desenvolvido por <i className="fa fa-code text-danger" /> 
+        <strong className="text-primary" > Diego Lima</strong>
+        </span>
     </footer>

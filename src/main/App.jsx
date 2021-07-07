b49d6d75cfe9bc1c906fc493d4e29c6f0c2c7727
Diego <lima.j.diego@gmail.com>
2021-07-07 11:25:19 -0300

    importando o bootstrap e font awesome

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './App.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
@@ -10,6 +13,6 @@ export default props =>
     <div className="app" >
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" title="Início" subtitle="Segundo Projeto em React" />
         <Footer />
     </div>

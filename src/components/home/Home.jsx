69652a248c3e151faabc3e9d6457fae0991c5199
Diego <lima.j.diego@gmail.com>
2021-07-07 15:01:47 -0300

    Exibindo o conteúdo pelo home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,2 +1,9 @@
 import React from 'react'
 import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Segundo Projeto em React">
+    <div className='display-4'>Bem vindo!</div>
+    <hr />
+    <p className="mb-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem minus sequi rem, cum quae quo, commodi aperiam nostrum corporis eum iure. Hic ab ipsa corrupti dolorem, veniam quae minus atque.</p>
+    </Main> 

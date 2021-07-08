f7ab7175e1c981b01b3795d915c733805e586f0d
Diego <lima.j.diego@gmail.com>
2021-07-08 08:42:22 -0300

    importando o componente de rota

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import './App.css'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
+import { HashRouter } from 'react-router-dom'
+import Routes from './Routes'
+
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
-    <div className="app" >
-        <Logo />
-        <Nav />
-        <Home />              
-        <Footer />
-    </div>
+    <HashRouter>
+        <div className="app" >
+            <Logo />
+            <Nav />
+            <Routes />              
+            <Footer />
+        </div>
+    </HashRouter>
+    
+   
